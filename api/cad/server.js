4f525f16f503631ada2c79e43a72910969d5c45e
Ujjwal Deep <mail2ujjwaldeephzb@gmail.com>
2026-05-27 17:24:36 -0400

    Fix docs hero asset and demo deployment

---
+++ api/cad/server.js
@@ -0,0 +1,5 @@
+import { handleHostedCadApi } from "../../viewer/src/server/vercelApi.mjs";
+
+export default async function handler(req, res) {
+  await handleHostedCadApi(req, res, { cadPath: "/__cad/server" });
+}
